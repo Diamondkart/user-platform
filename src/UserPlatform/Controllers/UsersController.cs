d2a9947a358254d82fe1fc74c6c4729e67fd6d56
Prakash Besra <Prakash.Besra@parentpay.com>
2023-09-20 21:50:57 +0530

    controller created

---
+++ src/UserPlatform/Controllers/UsersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserPlatform.Controllers
+{
+	[Route("v1/api/[controller]")]
+	[ApiController]
+	public class UsersController: ControllerBase
+	{
+	}
+}
