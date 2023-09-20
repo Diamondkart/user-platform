571f0020f67d4459e767767a6448f454790c99a4
Prakash Besra <Prakash.Besra@parentpay.com>
2023-09-20 20:55:23 +0530

    Generic api error format created

---
+++ src/UserPlatform/Utils/ResponseUtil.cs
@@ -0,0 +1,20 @@
+﻿namespace UserPlatform.Utils
+{
+	/// <summary>
+	/// Generic error message return format
+	/// </summary>
+	public class ResponseUtil
+	{
+		public static object ErrorResponse(string key, string value)
+		{
+			return new
+			{
+				Error = new Dictionary<string, object>()
+				{
+					{key, value },
+				}
+			};
+		}
+
+	}
+}
