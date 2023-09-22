573fcbad530e47fbb5a735520377c49803e8903a
MD Akram Alam <itsakramhere@gmail.com>
2023-09-22 19:35:59 +0530

    Added Usermodel and Dbcontext to get user list

---
+++ src/UserPlatform.DataInfra/Context/UserDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserPlatform.Model;
+
+namespace UserPlatform.DataModel
+{
+    public class UserDBContext:DbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options):base(options)
+        {
+            
+        }
+        public DbSet<UserDetails> Users{ get; set; }
+    }
+}
