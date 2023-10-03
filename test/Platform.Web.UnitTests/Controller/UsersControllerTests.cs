32738fd98e77de38ce565e0f918b404d90ec24ed
Prakash Besra <Prakash.Besra@parentpay.com>
2023-10-04 01:07:15 +0530

    [Prakash]: Unit tests added for Web

---
+++ test/Platform.Web.UnitTests/Controller/UsersControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using UserPlatform.ApplicationCore.Commands;
+using UserPlatform.ApplicationCore.Models.Request;
+using UserPlatform.ApplicationCore.Queries;
+using UserPlatform.Web.Controllers.v1;
+using UserPlatform.Web.Unit.Tests.Utilities;
+using Xunit;
+
+namespace UserPlatform.Web.Unit.Tests.Controller
+{
+    public class UsersControllerTests
+    {
+        private Mock<IQueryDispatcher> _queryDispatcherMock;
+        private Mock<ICommandDispatcher> _commandDispatcherMock;
+        private UsersController _userController;
+
+        public UsersControllerTests()
+        {
+            _queryDispatcherMock = new Mock<IQueryDispatcher>(MockBehavior.Strict);
+            _commandDispatcherMock = new Mock<ICommandDispatcher>(MockBehavior.Strict);
+            _userController = new UsersController(_commandDispatcherMock.Object, _queryDispatcherMock.Object);
+        }
+
+        [Fact]
+        public async Task GetUsers_HappyPath()
+        {
+            var userList = ObjectFactory.GetUserResponse.Generate(4);
+            CancellationToken cancellationToken = new CancellationToken();
+            _queryDispatcherMock
+                .Setup(x => x.QueryAsync(It.IsAny<GetUsersRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(userList);
+
+            var response = await _userController.GetUsers(cancellationToken) as ObjectResult;
+            Assert.NotNull(response);
+            Assert.Equal(StatusCodes.Status200OK, response.StatusCode);
+            Assert.Equal(userList, response.Value);
+        }
+    }
+}
---
+++ test/Platform.Web.UnitTests/Utilities/ObjectFactory.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using UserPlatform.ApplicationCore.Models.Response;
+
+namespace UserPlatform.Web.Unit.Tests.Utilities
+{
+    public class ObjectFactory
+    {
+        public static Faker<GetUsersResponse> GetUserResponse { get; } =
+            new Faker<GetUsersResponse>()
+            .RuleFor(x => x.UserId, y => y.Random.Guid())
+            .RuleFor(x => x.UserName, y => y.Random.Int().ToString())
+            .RuleFor(x => x.FirstName, y => y.Random.Int().ToString())
+            .RuleFor(x => x.LastName, y => y.Random.Int().ToString())
+            .RuleFor(x => x.MiddleName, y => y.Random.Int().ToString())
+            .RuleFor(x => x.MobileNo, y => y.Random.Int())
+            .RuleFor(x => x.Email, y => y.Random.Int().ToString() + "@example.com");
+    }
+}
